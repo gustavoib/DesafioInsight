8bd92852d304419e7e52b3951321bc226402bee8
gustavoib <joaogustavo831@gmail.com>
2024-06-06 15:02:48 -0300

    refactor: tostfy removido

--- desafio-insight-front/src/App.tsx
+++ desafio-insight-front/src/App.tsx
@@ -1,13 +1,10 @@
 import 'antd/dist/reset.css';
 import AppRoutes from './AppRoutes';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <AppRoutes />
-      <ToastContainer />
     </>
   )
 }
