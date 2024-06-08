a4d153555c8967e5284657b97df12cbcf8fb7932
gustavoib <joaogustavo831@gmail.com>
2024-06-08 00:50:16 -0300

    refactor: ajustes auth

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -3,12 +3,14 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { api, loginUser, register, getAll, getOne, del, update } from '../services/api.ts';
 
+// Criação do contexto AuthContext
 export const AuthContext = createContext({} as any);
 
+// Definição do componente AuthProvider 
+// O componente AuthProvider é um provedor de autenticação que permite o login e logout do usuário e o cadastro, listagem, edição e exclusão de fornecedores
 export const AuthProvider = ({children}:any) => {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState<boolean>(true); 
-    
+    const [loading, setLoading] = useState<boolean>(true);     
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -23,8 +25,7 @@ export const AuthProvider = ({children}:any) => {
     //função de login
     const login = async (email:string, password:string) => {
       try {  
-        const response = await loginUser(email, password);
-        
+        const response = await loginUser(email, password); 
         const loggedUser = response.data.user;
         const token = response.data.token
 
@@ -44,8 +45,6 @@ export const AuthProvider = ({children}:any) => {
 
     //função de logout
     const logout = () => {
-      console.log('logout');
-
       localStorage.removeItem('token');
       localStorage.removeItem('user');
       localStorage.removeItem('notes');
@@ -57,10 +56,7 @@ export const AuthProvider = ({children}:any) => {
     };
 
     //função de cadastro de fornecedor
-    const registerFornecedor = async (nome: string, cnpj: string, telefone: string, email: string, categoria: string, cep: string, rua: string, numero: string, bairro: string, cidade: string, estado: string) => {
-      const token = localStorage.getItem('token');
-      api.defaults.headers.Authorization = `Bearer ${token}`
-      
+    const registerFornecedor = async (nome: string, cnpj: string, telefone: string, email: string, categoria: string, cep: string, rua: string, numero: string, bairro: string, cidade: string, estado: string) => {      
       try {
         await register(nome, cnpj, telefone, email, categoria, cep, rua, numero, bairro, cidade, estado);
         
@@ -102,8 +98,6 @@ export const AuthProvider = ({children}:any) => {
 
     //função de exclusão de fornecedor
     const delFornecedor = async (id: string) => {
-
-      console.log(id)
       try {
         await del(id);
         return true;
